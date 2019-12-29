2a12fdb9876a95f38aeab6d77f7ee6329fdf016b
BestOfQ <3434614710@qq.com>
2019-12-29 10:10:31 +0800

    创建一些模板

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# 创建模板
+class Gallery(models.Models):
+	description = models.FileField(default='作品描述', max_length=100)
+	title = models.CharField(default='作品标题', max_length=50)
+	image = models.ImageField(default='default.jpg', upload_to='images/')
+		
